74eba2e59aba84e3f46c32107e9baf1203133da8
spark <fabricio.jacob@outlook.com>
2025-05-03 19:13:21 -0300

    add

--- mongodb-keyfile.sh
+++ mongodb-keyfile.sh
@@ -2,12 +2,12 @@
 
 # Cria um arquivo de chave seguro para autenticação do MongoDB replicaset
 echo "Criando arquivo de chave para MongoDB..."
-openssl rand -base64 756 > mongo-keyfile
-chmod 400 mongo-keyfile
+openssl rand -base64 756 > mongo-keyfile1
+chmod 400 mongo-keyfile1
 
 # Garantir que o Docker possa ler o arquivo
 echo "Ajustando permissões..."
-sudo chown 999:999 mongo-keyfile
+sudo chown 999:999 mongo-keyfile1
 
 echo "Arquivo de chave criado com sucesso: mongo-keyfile"
 echo "Agora você pode executar o docker-compose."
